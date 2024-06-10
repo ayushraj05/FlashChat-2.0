29728aa3751dcf899d3d216cda71891fba259dce
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-06-10 16:40:05 +0530

    add data to chatVC table view

--- FlashChat 2.0/Controller/ChatViewController.swift
+++ FlashChat 2.0/Controller/ChatViewController.swift
@@ -10,13 +10,27 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var messages: [Message] = [
+        Message(sender: "a@b.com", body: "Hey there!"),
+        Message(sender: "1@2.com", body: "yah!! 1@2 here"),
+        Message(sender: "a@b.com", body: "ohh! i'm a@b")
+    ]
+    
     override func viewDidLoad() {
+        TableView.dataSource = self
+//        TableView.delegate = self
         super.viewDidLoad()
         navigationItem.title = K.appName
         navigationItem.hidesBackButton = true
     }
     
-
+    @IBAction func SendMessageGotPressed(_ sender: UIButton) {
+        
+    }
+    
     @IBAction func LogOutGotPressed(_ sender: UIBarButtonItem) {
 
         do {
@@ -30,3 +44,21 @@ class ChatViewController: UIViewController {
     
 
 }
+
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+    
+}
+//
+//extension ChatViewController: UITableViewDelegate{
+//    
+//}
